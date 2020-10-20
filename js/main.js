d8614a3667ed259a1c9d256c90ead425255943dc
gio <giogirecd32@gmail.com>
2020-10-20 10:14:17 -0500

    no message

--- js/main.js
+++ js/main.js
@@ -28,7 +28,9 @@ function readObjet()
   xhttp.onreadystatechange = function(){
     if (this.readyState == 4 && this.status==200)
      {
-       console.log(this.responseText);
+       //console.log(this.responseText);
+       let datos = JSON.parse(this.responseText);
+       console.log(datos);
      }
   }
 }
