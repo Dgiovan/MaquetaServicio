604ed4c8e5076a81296ff6473e2e962a4fe878a1
gio <giogirecd32@gmail.com>
2020-10-21 18:49:16 -0500

    simples changes

--- js/main.js
+++ js/main.js
@@ -185,7 +185,7 @@ function showartistGalleryDetails(Artist)
 {
   
     nameArtist.textContent = Artist;
-    nameArtist.classList.add("texcolorBlackbold");
+    nameArtist.classList.add("texcolorBlackbold","animationNameAuthor");
 
     decriptionArtist.textContent ="Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?";
     decriptionArtist.classList.add("texcolorBlackbold","simpleMargin");  
@@ -219,10 +219,10 @@ function showartistGalleryDetails(Artist)
               var medidas    = document.createElement("p");
                   medidas.classList.add("meta"); 
               var decriptionWork = document.createElement("p");
-                  decriptionWork.classList.add("card-text","background_black"); 
+                  decriptionWork.classList.add("card-text"); 
 
               var footerCard = document.createElement("div");
-                  footerCard.classList.add("card-footer");     
+                  footerCard.classList.add("card-footer","background_black");     
 
               var seeWork    = document.createElement("small");  
               var buttonwork = document.createElement("a");  
